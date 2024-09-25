d3b2558517dfa447a54cfe3f340cec1603c6601a
Amreet Bains <amreet8@hotmail.co.uk>
2024-09-25 14:20:23 +0100

    added seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating Restaraunts"
+pizza = { name: "Pizza World", address: "London", category: "italian" }
+chinese = { name: "Taste of China", address: "Paris", category: "chinese" }
+sushi = { name: "Sushi Land", address: "Rome", category: "japanese" }
+french = { name: "Taste of France", address: "Berlin", category: "french" }
+belgian = { name: "Belgian Buns", address: "Amsterdam", category: "belgian" }
+
+[ pizza, chinese, sushi, french, belgian ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
